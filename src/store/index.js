cc2ea0cb8a5c809ac4cc173e87370cb297edcf21
Araceli Sánchez <aracelisanchezisc@gmail.com>
2020-08-04 12:24:01 -0700

    State and Getters lesson.

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,35 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 1, name: 'Araceli Sanchez' },
+    categories: ['sustainability', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: 'abc', done: false },
+      { id: 2, text: 'def', done: false },
+      { id: 3, text: 'ghi', done: false },
+      { id: 4, text: 'jkl', done: true },
+      { id: 5, text: 'mnn', done: true }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    // This is in case we want to access to this value from multiple values in our app
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    // Getter inside of getter
+    activeTodos: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    // Getter using parameter id
+    getTodoById: state => id => {
+      return state.todos.find(event => event.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,56 @@
 <template>
-  <h1>Create event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories:</p>
+
+    <ul>
+      <li v-for="(cat, index) in categories" :key="index">{{ cat }}</li>
+    </ul>
+
+    <p>{{ getTodoById(1) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed:
+    //   {
+    //     userName() {
+    //       return this.$store.state.user.name
+    //     }
+    //   }
+
+    // mapState({
+    //   userName: state => state.user.name,
+    //   userID: state => state.user.id,
+    //   categories: state => state.categories
+    // })
+
+    // // simplest way to do mapping
+    // mapState({
+    //   user: 'user',
+    //   categories: 'categories'
+    // })
+
+    // Other way to do it
+    // computed: mapState['user', 'categories']
+
+    // Latest, in case we want to declare also a local computed method
+    {
+      catLength() {
+        return this.$store.getters.catLength
+      },
+      ...mapState(['user', 'categories']),
+      //   getTodo() {
+      //     return this.$store.getters.getTodoById
+      //   }
+      // getting todos with mapGetters
+      ...mapGetters(['getTodoById'])
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
