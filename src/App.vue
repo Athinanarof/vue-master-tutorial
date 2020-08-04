70d5fadb94805834a1d4f0df1ee10baf1c11283d
Araceli Sánchez <aracelisanchezisc@gmail.com>
2020-08-03 17:13:23 -0700

    Playing around with the router.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- component to create a link -->
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'About' }">About</router-link>
     </div>
+    <!-- Here it will be render the component -->
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router' // Import our router.js -> This is to navitage in different pages in our app
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  router,
+  router, // ES6 equivalent to router: router
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
